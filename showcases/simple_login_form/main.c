6e32f300495e123d28863e33117b02ebb17851a0
SwirX <swirxytb@gmail.com>
2025-12-30 22:37:06 +0100

    feat(showcase): initialize sxui engine and window

--- showcases/simple_login_form/main.c
+++ showcases/simple_login_form/main.c
@@ -2,6 +2,20 @@
 #include "sxui.h"
 #include "dynamic_list.h"
 
+int WIDTH = 1200;
+int HEIGHT = 720;
+
 int main() {
-    printf("Hello World!");
+    sxui_init("Simple Login Form", WIDTH, HEIGHT, 0xFF5A5AFF);
+
+    UIElement* hello_world = sxui_label(NULL, "Hello World!");
+    sxui_set_position(hello_world, 50, 10);
+
+    while(!sxui_should_quit()) {
+        sxui_poll_events();
+        sxui_render();
+    }
+
+    sxui_cleanup();
+    return 0;
 }
