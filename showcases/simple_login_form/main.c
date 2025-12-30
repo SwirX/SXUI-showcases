dfb9129a5a89d0015baf8ed4905e53030d9bb975
SwirX <swirxytb@gmail.com>
2025-12-30 23:04:23 +0100

    feat(showcase): add required UI elements

--- showcases/simple_login_form/main.c
+++ showcases/simple_login_form/main.c
@@ -8,8 +8,19 @@ int HEIGHT = 720;
 int main() {
     sxui_init("Simple Login Form", WIDTH, HEIGHT, 0xFF5A5AFF);
 
-    UIElement* hello_world = sxui_label(NULL, "Hello World!");
-    sxui_set_position(hello_world, 50, 10);
+    int form_w = 400;
+    int form_h = 500;
+    UIElement* login_form_bg = sxui_frame(NULL, (WIDTH-form_w)/2, (HEIGHT-form_h)/2, form_w, form_h, UI_FLAG_NONE);
+
+    UIElement* login_form_text = sxui_label(login_form_bg, "Login");
+    
+    UIElement* login_username_input = sxui_input(login_form_bg, "Enter Your Username", 0);
+
+    UIElement* login_password_input = sxui_input(login_form_bg, "Enter Your Password", 0);
+    
+    UIElement* login_password_toggle = sxui_button(login_form_bg, "Toggle Password", NULL);
+
+    UIElement* login_button = sxui_button(login_form_bg, "Login", NULL);
 
     while(!sxui_should_quit()) {
         sxui_poll_events();
